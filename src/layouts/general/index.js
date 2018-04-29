93f6bbcb22e1590b2ee41050f3cf6a33809aed4d
Daniel Thompson <dthompson844@gmail.com>
2018-04-29 03:33:47 -0500

    Created layout components for index page

---
+++ src/layouts/general/index.js
@@ -0,0 +1,34 @@
+import glamorous from 'glamorous'
+
+export const LogoLayout = glamorous.div({
+  flex: 1,
+  margin: '3rem',
+})
+
+export const ContentLayout = glamorous.div({
+  margin: '1.5rem',
+  alignSelf: 'center',
+})
+
+export const MainLayout = glamorous.div({
+  backgroundColor: 'purple',
+
+  maxWidth: '960px',
+
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const BodyLayout = glamorous.div({
+  backgroundColor: '#37394C',
+
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  overflow: 'hidden',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
