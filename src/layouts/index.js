ac72da9d658d11e304ace15fd3c856aa8604f845
Daniel Thompson <dthompson844@gmail.com>
2018-04-29 03:34:32 -0500

    updated the index layout with new layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,36 +1,52 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+
 import 'glamor/reset'
 
-const Layout = ({ children, data }) => {
+import Logo from 'components/logo'
+import {
+  BodyLayout,
+  MainLayout,
+  LogoLayout,
+  ContentLayout,
+} from 'layouts/general'
+
+const RootLayout = ({ children, data }) => {
   const { title, description, keywords } = data.site.siteMetadata
 
   return (
-    <div>
-      <Helmet
-        title={title}
-        meta={[
-          {
-            name: 'description',
-            content: description,
-          },
-          {
-            name: 'keywords',
-            content: keywords,
-          },
-        ]}
-      />
-      {children()}
-    </div>
+    <BodyLayout>
+      <MainLayout>
+        <Helmet
+          title={title}
+          meta={[
+            {
+              name: 'description',
+              content: description,
+            },
+            {
+              name: 'keywords',
+              content: keywords,
+            },
+          ]}
+        />
+
+        <LogoLayout>
+          <Logo />
+        </LogoLayout>
+
+        <ContentLayout>{children()}</ContentLayout>
+      </MainLayout>
+    </BodyLayout>
   )
 }
 
-Layout.propTypes = {
+RootLayout.propTypes = {
   children: PropTypes.func,
 }
 
-export default Layout
+export default RootLayout
 
 export const query = graphql`
   query SiteTitleQuery {
