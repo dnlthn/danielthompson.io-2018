e4b8444dea9aecb3f07585a5228ef7831f8d4806
Daniel Thompson <dthompson844@gmail.com>
2018-04-29 06:19:44 -0500

    Created a nav icon component

---
+++ src/components/nav-icon/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import glamorous from 'glamorous'
+import Link from 'gatsby-link'
+
+import Tooltip from 'components/tooltip'
+
+const NavIcon = ({ path, label, svg, isReleased }) => {
+  // if (!isReleased) return <div />
+
+  return path.startsWith('/') ? (
+    <Link
+      to={path}
+      activeStyle={{ borderBottom: '3px solid #FECA57', borderRadius: '1px' }}
+    >
+      <Tooltip text={label}>
+        <Image src={svg} alt={`Navigate to the ${label} page`} />
+      </Tooltip>
+    </Link>
+  ) : (
+    <a href={path}>
+      <Tooltip text={label}>
+        <Image src={svg} alt={`Navigate to the ${label} page`} />
+      </Tooltip>
+    </a>
+  )
+}
+
+const Image = glamorous.img({
+  height: 'auto',
+  width: '100%',
+})
+
+export default NavIcon
