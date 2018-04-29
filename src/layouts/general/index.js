e1951d4adf4ee42ee26bcd3b1a7b69d9930c6705
Daniel Thompson <dthompson844@gmail.com>
2018-04-29 06:18:10 -0500

    Cleaned up  un-needed css

--- src/layouts/general/index.js
+++ src/layouts/general/index.js
@@ -2,35 +2,33 @@ import glamorous from 'glamorous'
 
 export const LogoLayout = glamorous.div({
   flex: 1,
-  margin: '3rem',
 })
 
 export const ContentLayout = glamorous.div({
-  margin: '1.5rem',
   alignSelf: 'center',
 })
 
 export const MainLayout = glamorous.div({
   // TEMP: Temporary color to see the div without
   // having to use devtools
-  backgroundColor: 'purple',
+  // backgroundColor: 'purple',
 
-  maxWidth: '960px',
-
-  flex: 1,
   display: 'flex',
+  flex: 1,
   flexDirection: 'column',
+  maxWidth: '960px',
+  padding: '2rem',
 })
 
 export const BodyLayout = glamorous.div({
-  backgroundColor: '#37394C',
-
-  width: '100%',
-  height: '100%',
-  position: 'absolute',
-  overflow: 'hidden',
-
-  display: 'flex',
   alignItems: 'center',
+  backgroundColor: '#37394C',
+  display: 'flex',
+  height: '100%',
   justifyContent: 'center',
+  overflow: 'hidden',
+  position: 'absolute',
+  width: '100%',
+  fontFamily:
+    'Titillium Web, -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
 })
