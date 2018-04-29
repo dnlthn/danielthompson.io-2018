4e9d63ed5a4d053a3ddf49e60ac930c7f1d93ced
Daniel Thompson <dthompson844@gmail.com>
2018-04-29 03:34:49 -0500

    cleaned up un-needed pages and text

--- src/components/header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Header = () => <p>Header World!</p>
-
-export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const IndexPage = () => <div>Index World!</div>
+const IndexPage = () => <div />
 
 export default IndexPage
