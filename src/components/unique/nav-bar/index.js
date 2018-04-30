37e065282d69f45b284bdb7e1171158fd7383154
Daniel Thompson <dthompson844@gmail.com>
2018-04-30 05:07:47 -0500

    Added working navbar component

---
+++ src/components/unique/nav-bar/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import NavIcon from 'components/reusable/nav-icon'
+import navigation from 'data/nav'
+import glamorous from 'glamorous'
+
+const NavBar = () => (
+  <Container>
+    {navigation.map(nav_item => {
+      if (!nav_item.isReleased) return null
+      return <NavIcon key={nav_item.label} {...nav_item} />
+    })}
+  </Container>
+)
+
+const number_of_displayed_icons = navigation.filter(
+  nav_item => nav_item.isReleased
+).length
+
+const Container = glamorous.div({
+  alignItems: 'center',
+  display: 'grid',
+  gridGap: '1rem',
+  gridTemplateColumns: `repeat(${number_of_displayed_icons}, 20px)`,
+  justifyContent: 'end',
+  marginTop: '.5rem',
+
+  '@media(min-width: 576px)': {
+    gridGap: '1.5rem',
+    gridTemplateColumns: `repeat(${number_of_displayed_icons}, 25px)`,
+  },
+})
+
+export default NavBar
