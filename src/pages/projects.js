fdccdd8b79da3508316522d49acc8696c96b166e
Daniel Thompson <dthompson844@gmail.com>
2018-04-30 05:11:42 -0500

    Added projects and skills page skeletons

---
+++ src/pages/projects.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Projects = () => <div>Hello Projects!</div>
+
+export default Projects
---
+++ src/pages/skills.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Skills = () => <div>Hello Skills!</div>
+
+export default Skills
