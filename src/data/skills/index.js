a42a1cf44761265d99e9d3e4b790de49e87688e9
Daniel Thompson <dthompson844@gmail.com>
2018-05-01 23:48:00 -0500

    Added data for previous tech

--- src/data/skills/index.js
+++ src/data/skills/index.js
@@ -35,6 +35,22 @@ import ruby from 'images/tech-icons/ruby.svg'
 
 import digitalocean from 'images/tech-icons/digitalocean.svg'
 
+// Previous Tech
+import angular from 'images/tech-icons/angular.svg'
+import bootstrap from 'images/tech-icons/bootstrap.svg'
+import jquery from 'images/tech-icons/jquery.svg'
+import sass from 'images/tech-icons/sass.svg'
+import dotnet from 'images/tech-icons/dotnet.svg'
+
+import python from 'images/tech-icons/python.svg'
+import java from 'images/tech-icons/java.svg'
+import c from 'images/tech-icons/c.svg'
+import cplusplus from 'images/tech-icons/cplusplus.svg'
+import lua from 'images/tech-icons/lua.svg'
+
+import atom from 'images/tech-icons/atom.svg'
+import intellij from 'images/tech-icons/intellij.svg'
+
 const daily_tech = [
   { label: 'React', svg: react, frequency: 'daily', category: 'tooling' },
   { label: 'Apollo', svg: apollo, frequency: 'daily', category: 'tooling' },
@@ -139,4 +155,80 @@ const occasional_tech = [
   },
 ]
 
-export default [...daily_tech, ...occasional_tech]
+const previous_tech = [
+  {
+    label: 'Angular.js',
+    svg: angular,
+    frequency: 'previously',
+    category: 'tooling',
+  },
+  {
+    label: 'Bootstrap',
+    svg: bootstrap,
+    frequency: 'previously',
+    category: 'tooling',
+  },
+  {
+    label: 'jQuery',
+    svg: jquery,
+    frequency: 'previously',
+    category: 'tooling',
+  },
+  {
+    label: 'Sass',
+    svg: sass,
+    frequency: 'previously',
+    category: 'tooling',
+  },
+  {
+    label: '.NET',
+    svg: dotnet,
+    frequency: 'previously',
+    category: 'tooling',
+  },
+
+  {
+    label: 'Python',
+    svg: python,
+    frequency: 'previously',
+    category: 'languages',
+  },
+  {
+    label: 'Java',
+    svg: java,
+    frequency: 'previously',
+    category: 'languages',
+  },
+  {
+    label: 'C',
+    svg: c,
+    frequency: 'previously',
+    category: 'languages',
+  },
+  {
+    label: 'C++',
+    svg: cplusplus,
+    frequency: 'previously',
+    category: 'languages',
+  },
+  {
+    label: 'Lua',
+    svg: lua,
+    frequency: 'previously',
+    category: 'languages',
+  },
+
+  {
+    label: 'Atom',
+    svg: atom,
+    frequency: 'previously',
+    category: 'the rest',
+  },
+  {
+    label: 'Intellij IDEA',
+    svg: intellij,
+    frequency: 'previously',
+    category: 'the rest',
+  },
+]
+export default [...daily_tech, ...occasional_tech, ...previous_tech]
