eec7210684c63a0073004d9c2d453dde7b59f50e
Daniel Thompson <dthompson844@gmail.com>
2018-05-01 23:14:50 -0500

    Added the dropdown render prop to the skill page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,15 +2,29 @@ import React from 'react'
 import glamorous from 'glamorous'
 
 import SkillsList from 'components/templates/skills-list'
+import Dropdown from 'containers/dropdown'
 const Skills = () => (
-  <div>
-    <Statement>
-      Here's some tech I use <Frequency>daily</Frequency>
-    </Statement>
-    <SkillsList frequency="daily" category="tooling" />
-    <SkillsList frequency="daily" category="languages" />
-    <SkillsList frequency="daily" category="the rest" />
-  </div>
+  <Dropdown initialValue="daily">
+    {({ value, handleChange }) => {
+      return (
+        <div>
+          <Statement>
+            Here's some tech I use{' '}
+            <DropdownWrapper>
+              <FrequencyDropdown value={value} onChange={handleChange}>
+                <option value="daily">daily</option>
+                <option value="occasionally">occasionally</option>
+                <option value="previously">previously</option>
+              </FrequencyDropdown>
+            </DropdownWrapper>
+          </Statement>
+          <SkillsList frequency={value} category="tooling" />
+          <SkillsList frequency={value} category="languages" />
+          <SkillsList frequency={value} category="the rest" />
+        </div>
+      )
+    }}
+  </Dropdown>
 )
 
 const Statement = glamorous.h1({
@@ -24,9 +38,17 @@ const Statement = glamorous.h1({
   },
 })
 
-const Frequency = glamorous.span({
+const DropdownWrapper = glamorous.span({
+  borderBottom: '2px dashed #FECA57',
+})
+
+const FrequencyDropdown = glamorous.select({
   fontWeight: '600',
   color: '#FECA57',
+  backgroundColor: 'transparent',
+  border: 'none',
+  borderRadius: '0',
+  paddingBottom: '.25rem',
 })
 
 export default Skills
